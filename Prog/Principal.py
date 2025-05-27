1a156219ddd5dc1f5f230267628728d62216b61d
lauraamante <lauraoliveira.amante@gmail.com>
2025-05-26 22:02:47 -0300

    Principal.py

---
+++ Prog/Principal.py
@@ -0,0 +1,10 @@
+from Janela import Janela
+
+Jan1=Janela("Consumo de Automóveis")
+Jan1.mainloop()
+##################################################
+
+## Questão 12:  (Aloque um objeto do classe Janela e mostre-o na tela)
+
+
+##################################################
